80110a2871f8de521f1a8871c6a5c9c83a880e9d
Radzeer <hollo.attila.82@gmail.com>
2022-06-22 16:46:57 +0200

    var :)

---
+++ src/main/java/variable/Var.java
@@ -0,0 +1,11 @@
+package variable;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Var {
+
+    public static void main(String[] args) {
+        var var=new Var();
+    }
+}
